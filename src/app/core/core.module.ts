5cb2ab911e353879994f95d95c1534f8b76025ee
Aivars Liepa <aivarsliepa1992@gmail.com>
2019-03-16 18:53:30 +0200

    angular/flex-layout added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,9 +1,10 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
 
 @NgModule({
   declarations: [],
-  imports: [BrowserModule, BrowserAnimationsModule]
+  imports: [BrowserModule, BrowserAnimationsModule, FlexLayoutModule]
 })
 export class CoreModule {}
