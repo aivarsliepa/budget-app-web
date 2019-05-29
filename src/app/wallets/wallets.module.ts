f768282c09571a40f0ee0a945b9c2a8bf47e0a20
Aivars Liepa <aivarsliepa1992@gmail.com>
2019-05-29 22:25:51 +0300

    rename

--- src/app/wallets/wallets.module.ts
+++ src/app/wallets/wallets.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from "@angular/core";
 import { NewWalletDialogComponent } from "./new-wallet-dialog/new-wallet-dialog.component";
 import { WalletListItemComponent } from "./wallet-list-item/wallet-list-item.component";
 import { WalletListComponent } from "./wallet-list/wallet-list.component";
-import { WalletRoutingModule } from "./wallets-routing.module";
+import { WalletsRoutingModule } from "./wallets-routing.module";
 import { SharedModule } from "src/app/shared/shared.module";
 
 @NgModule({
-  imports: [SharedModule, WalletRoutingModule],
+  imports: [SharedModule, WalletsRoutingModule],
   declarations: [WalletListComponent, WalletListItemComponent, NewWalletDialogComponent],
   entryComponents: [NewWalletDialogComponent]
 })
-export class WalletModule {}
+export class WalletsModule {}
